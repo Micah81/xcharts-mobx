316d5866f867a69dc86e847e883e9e6e42cb6771
Micah81 <micahmrocks@gmail.com>
2018-08-02 21:30:59 -0600

    Minor edits

--- src/components/anonSymbols.js
+++ src/components/anonSymbols.js
@@ -4,16 +4,7 @@ import * as ts from '../utils/robinhood/topStocks';
 
 module.exports = {
   anonSymbols: function () {
-    console.log('here I am!')
-    ts.TopStocks(creds.credentials)
+    let anonSymbolsArray = []
+    anonSymbolsArray = ts.TopStocks(creds.credentials)
   }
 }
-
-/*
-
-Pass the symbol array to the state.
-Add the chart component.
-Put the symbol array into the chart.
-When they push the button to vote, pass the symbol via the store.  
-
-*/
